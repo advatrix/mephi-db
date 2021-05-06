e43d8ce10ec5bf2d98af03cc6a7c86def0f86dc5
advatrix <trojan.advatrix@gmail.com>
2021-05-06 10:50:10 +0300

    query 2

--- queries.sql
+++ queries.sql
@@ -26,4 +26,83 @@ from pricelist_category pc
     left join consignment_note cn on c.id = cn.customer_id
     left join active_customers ac on ac.pc_id = pc.id
 group by pc.id, ac.count, tc.count
-order by 2 desc;
+order by 2 desc, 3 desc;
+
+
+select
+    c.id as "customer id",
+    c.code as "customer code",
+    c.registered as "registration date",
+    l.inn as "inn",
+    l.payment_account as "payment account",
+    p.first_name as "first name",
+    p.second_name as "second name",
+    p.last_name as "last name",
+    c.type as "customer type",
+    coalesce(registered_count, 0) as "notes registered",
+    coalesce(potential_income, 0) as "potential income",
+    coalesce(canceled_count, 0) as "notes canceled",
+    coalesce(unearned_income, 0) as "unearned income",
+    coalesce(unpaid_count, 0) as "notes unpaid",
+    coalesce(debt, 0) as "debt"
+from customer c
+    left join legal l on c.id = l.id
+    left join person p on c.id = p.id
+    left join (
+        select
+            cn.customer_id as customer_id,
+            count(cn.id) as registered_count,
+            sum(item_info.item_price) as potential_income
+        from consignment_note cn
+            left join (
+                select
+                    cni.consignment_note_id as cn_id,
+                    sum(cni.ordered * pi.price) as item_price
+                from consignment_note_item cni
+                    join pricelist_item pi on cni.pricelist_id = pi.pricelist_id and cni.good_id = pi.good_id
+                group by cn_id
+            ) as item_info on item_info.cn_id = cn.id
+        where not cn.is_canceled
+        group by cn.customer_id
+    ) as registered_notes on registered_notes.customer_id = c.id
+    left join (
+        select
+            cn.customer_id as customer_id,
+            count(cn.id) as canceled_count,
+            sum(item_info.item_price) as unearned_income
+        from consignment_note cn
+            left join (
+                select
+                    cni.consignment_note_id as cn_id,
+                    sum(cni.ordered * pi.price) as item_price
+                from consignment_note_item cni
+                    join pricelist_item pi on cni.pricelist_id = pi.pricelist_id and cni.good_id = pi.good_id
+                group by cn_id
+            ) as item_info on item_info.cn_id = cn.id
+        where cn.is_canceled
+        group by cn.customer_id
+    ) as canceled_notes on canceled_notes.customer_id = c.id
+    left join (
+        select
+            cn.customer_id as customer_id,
+            count(cn.id) as unpaid_count,
+            sum(item_info.item_price) - sum(payment_sum) as debt
+        from consignment_note cn
+            left join (
+                select
+                    cni.consignment_note_id as cn_id,
+                    sum(cni.ordered * pi.price) as item_price
+                from consignment_note_item cni
+                    join pricelist_item pi on cni.pricelist_id = pi.pricelist_id and cni.good_id = pi.good_id
+                group by cn_id
+            ) as item_info on item_info.cn_id = cn.id
+            left join (
+                select
+                    consignment_note_id as cn_id,
+                    sum(payment) as payment_sum
+                from payment_document
+                group by consignment_note_id
+            ) as payment_info on payment_info.cn_id = cn.id
+        where cn.payment_document_id is null
+        group by cn.customer_id
+    ) as unpaid_notes on unpaid_notes.customer_id = c.id;
